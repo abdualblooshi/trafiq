8eabcfcdef454ed2802aee4635632ce77dad5ee8
Abdulrahman Alblooshi <aboodshadow@outlook.com>
2024-11-21 00:13:50 +0400

    Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./public/**/*.{html,js}",
+    "./views/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./index.html", // Add this if your index.html is in root
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
